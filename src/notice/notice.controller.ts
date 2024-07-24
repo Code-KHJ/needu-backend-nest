70e514c22e1eb22e4070458435252031773f0b00
DESKTOP-0BC1261\GAION <hjk@gaion.kr>
2024-07-24 09:44:54 +0900

    notice: feat update

--- src/notice/notice.controller.ts
+++ src/notice/notice.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { NoticeService } from './notice.service';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { RolesGuard } from 'src/common/guards/roles.guard';
@@ -6,6 +6,7 @@ import { RoleType } from 'src/common/role-type';
 import { Roles } from 'src/common/decorators/role.decorator';
 import { GetCurrentUser } from 'src/common/decorators';
 import { NoticeCreateDto } from './dto/notice-create.dto';
+import { NoticeUpdateDto } from './dto/notice-update.dto';
 
 @Controller('notice')
 export class NoticeController {
@@ -29,5 +30,27 @@ export class NoticeController {
   @Roles(RoleType.ADMIN)
   @Get('/edit/:id')
   @ApiOperation({ summary: '공지사항 수정용 조회' })
-  async getNoticeForEdit(@GetCurrentUser('id') userId: number) {}
+  @ApiResponse({
+    status: 200,
+    description: '조회 완료',
+  })
+  async getNoticeForEdit(@GetCurrentUser('id') userId: number, @Param('id') noticeId: number) {
+    const response = await this.noticeService.getNoticeForEdit(userId, noticeId);
+
+    return response;
+  }
+
+  @UseGuards(RolesGuard)
+  @Roles(RoleType.ADMIN)
+  @Patch('/edit/:id')
+  @ApiOperation({ summary: '공지사항 수정' })
+  @ApiResponse({
+    status: 201,
+    description: '공지사항 수정',
+  })
+  async updateNotice(@GetCurrentUser('id') userId: number, @Param('id') postId: number, @Body() noticeUpdateDto: NoticeUpdateDto) {
+    const response = await this.noticeService.updateNotice(userId, noticeUpdateDto);
+
+    return response;
+  }
 }
